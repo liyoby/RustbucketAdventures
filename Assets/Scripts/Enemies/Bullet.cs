9c3280e1e62b2b568e349f44275c2b62d5408571
liyoby <kazuspirit@gmail.com>
2020-02-18 15:54:16 -0600

    Enemy Bullet Changes
    +Large Enemy shoots in straight line, long range
    +Bullet destroys on Player contact
    +Large Enemy moves towards Player when "seen"

--- Assets/Scripts/Enemies/Bullet.cs
+++ Assets/Scripts/Enemies/Bullet.cs
@@ -6,52 +6,37 @@ public class Bullet : MonoBehaviour
 {
     public float speed;
 
-    public bool inRange = false;
-
-    public Transform pathStart,          //Assign gameObjs to these variables for manipulation
-                     pathEnd;
-
-    private Transform path;
+    private Transform player;
     private Vector2 target;
 
     void Start()
     {
-        path = GameObject.FindGameObjectWithTag("Path End").transform;
+        player = GameObject.FindGameObjectWithTag("Path End").transform;
 
-        target = new Vector2(path.position.x, path.position.y);
+        target = new Vector2(player.position.x, player.position.y);
     }
 
     void Update()
     {
-
-        if (playerInRange())
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
-        }
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
 
         if (transform.position.x == target.x && transform.position.y == target.y)
         {
             destroyBullet();
         }
 
-    }
-
-    public bool playerInRange()
-    {
-        return (inRange = Physics2D.Linecast(pathStart.position, pathEnd.position, 1 << LayerMask.NameToLayer("Player")));
-    }
-
-    void destroyBullet()
-    {
-        Destroy(gameObject);
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
+        void destroyBullet()
         {
-            destroyBullet();
+            Destroy(gameObject);
         }
 
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                destroyBullet();
+            }
+
+        }
     }
 }
