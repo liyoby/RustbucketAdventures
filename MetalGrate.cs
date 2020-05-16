a498b42f5029770333b967bb8bd4365eb2f91df3
liyoby <42914892+liyoby@users.noreply.github.com>
2020-05-16 15:18:15 -0500

    Add files via upload
    +Activate grates through breaker box
    +Move grates
    +Instantiate grates
    
    Requires 2 empty game objects.  1 for start point. 1 for end point.  
    
    Specify Path end through Path Tag (string name) for end point.   Check that all end points have different tags, to ensure grates do not travel wrong path.
    
    Going to make some prefabs for grates.  You need different orientations of the image for grate, because it does not automatically orientate it correctly.   I'll do those prefabs and drop them in Google Drive real quick.

---
+++ MetalGrate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalGrate : MonoBehaviour
+{
+
+    public float speed;
+
+    public string pathTag;
+
+    private Transform grate;
+    private Vector2 target;
+
+    void Start()
+    {
+        grate = GameObject.FindGameObjectWithTag(pathTag).transform;
+
+        target = new Vector2(grate.position.x, grate.position.y);
+    }
+
+    public void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            destroyGrate();
+        }
+    }
+
+    void destroyGrate()
+    {
+        Destroy(gameObject);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            destroyGrate();
+        }
+
+    }
+}
---
+++ MetalGrateMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalGrateMovement : MonoBehaviour
+{
+
+    public float timeTweenShots;
+    public float startTime;
+
+    string eBall = "Electro Ball";
+
+    public GameObject grate;
+    public GameObject bBox;
+
+
+    void Start()
+    {
+        timeTweenShots = startTime; //At start, set start time to count down from.
+    }
+
+    void Update()
+    {
+        moveGrate();
+    }
+
+    public void moveGrate()
+    {
+        if (timeTweenShots <= 0)
+        {
+            Instantiate(grate, transform.position, Quaternion.identity);
+            timeTweenShots = startTime;   //Reset time between shots to a set start time.
+        }
+        else
+        {
+            //Ticks down time between shots.  Enemy shoots on 0.
+            timeTweenShots -= Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter2d(Collider2D bBox)
+    {
+        if (bBox.CompareTag(eBall))
+        {
+            moveGrate();
+        }
+    }
+}
